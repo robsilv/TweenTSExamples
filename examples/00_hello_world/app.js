33c5c7e3a28232c8afe4fbcec88320c53a1c1acf
Robert Silverton <rob.silverton@gmail.com>
2014-06-13 16:44:20 +1200

    HelloWorld example added

---
+++ examples/00_hello_world/app.js
@@ -0,0 +1,39 @@
+﻿window.onload = function () {
+    var example = new HelloWorld();
+    example.init();
+    example.animate();
+};
+
+var HelloWorld = (function () {
+    function HelloWorld() {
+        var _this = this;
+        this.animate = function () {
+            requestAnimationFrame(_this.animate);
+
+            TWEEN.update();
+        };
+        this.update = function () {
+            _this.target.style.left = _this.position.x + 'px';
+            _this.target.style.top = _this.position.y + 'px';
+
+            // Needs to be dynamic to access vars that potentially don't exist:
+            var targ = _this.target;
+            targ.style.webkitTransform = 'rotate(' + Math.floor(_this.position.rotation) + 'deg)';
+            targ.style.MozTransform = 'rotate(' + Math.floor(_this.position.rotation) + 'deg)';
+        };
+    }
+    HelloWorld.prototype.init = function () {
+        this.position = { x: 100, y: 100, rotation: 0 };
+        this.target = document.getElementById('target');
+        this.tween = new TWEEN.Tween(this.position).to({ x: 700, y: 200, rotation: 359 }, 2000).delay(1000).easing(TWEEN.Easing.Elastic.InOut).onUpdate(this.update);
+
+        this.tweenBack = new TWEEN.Tween(this.position).to({ x: 100, y: 100, rotation: 0 }, 3000).easing(TWEEN.Easing.Elastic.InOut).onUpdate(this.update);
+
+        this.tween.chain(this.tweenBack);
+        this.tweenBack.chain(this.tween);
+
+        this.tween.start();
+    };
+    return HelloWorld;
+})();
+//# sourceMappingURL=app.js.map
---
+++ examples/00_hello_world/app.ts
@@ -0,0 +1,54 @@
+﻿window.onload = () => {
+    var example = new HelloWorld();
+    example.init();
+    example.animate();
+};
+
+class HelloWorld
+{
+    position: any;
+    target: HTMLDivElement;
+    tween: TWEEN.Tween;
+    tweenBack: TWEEN.Tween;
+
+    public init():void {
+
+        this.position = { x: 100, y: 100, rotation: 0 };
+        this.target = <HTMLDivElement>document.getElementById('target');
+        this.tween = new TWEEN.Tween(this.position)
+            .to({ x: 700, y: 200, rotation: 359 }, 2000)
+            .delay(1000)
+            .easing(TWEEN.Easing.Elastic.InOut)
+            .onUpdate(this.update);
+
+        this.tweenBack = new TWEEN.Tween(this.position)
+            .to({ x: 100, y: 100, rotation: 0 }, 3000)
+            .easing(TWEEN.Easing.Elastic.InOut)
+            .onUpdate(this.update);
+
+        this.tween.chain(this.tweenBack);
+        this.tweenBack.chain(this.tween);
+
+        this.tween.start();
+
+    }
+
+    public animate = ():void => {
+
+        requestAnimationFrame(this.animate);
+
+        TWEEN.update();
+
+    }
+
+    public update = ():void => {
+
+        this.target.style.left = this.position.x + 'px';
+        this.target.style.top = this.position.y + 'px';
+        // Needs to be dynamic to access vars that potentially don't exist:
+        var targ: any = <any>this.target;
+        targ.style.webkitTransform = 'rotate(' + Math.floor(this.position.rotation) + 'deg)';
+        targ.style.MozTransform = 'rotate(' + Math.floor(this.position.rotation) + 'deg)';
+
+    }
+}
